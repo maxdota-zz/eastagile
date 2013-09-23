c67b87ae7a4b56c85a630ad68f252d88c8b9f0b2
maxdota <sieu_ngoc_2007@yahoo.com>
2013-09-23 17:05:28 +0700

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ngoc::Application.config.secret_token = '8b5315135e8e4a06704df3094ffa1f1c5fcc394fb8f7ade2b52a8774ea18b1298c99d62c95a3542f652c1305735d4c33badfc221dc361d1490b349bb9b0e9192'
